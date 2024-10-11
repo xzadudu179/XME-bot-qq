d94f43df0ecb874ef71d0aadd80da97c5ad9efdd
xzadudu179 <terra179@163.com>
2024-10-12 03:30:44 +0800

    update

--- xme/plugins/notice.py
+++ xme/plugins/notice.py
@@ -9,8 +9,11 @@ async def _(session: NoticeSession):
         settings = json.load(jsonfile)
     recalled_message = await session.bot.api.get_msg(message_id=session.event['message_id'])
     log.logger.info(f"消息被撤回，内容为：{recalled_message['message']}")
-    if settings['prevent_recall'].get(str(session.event.group_id), False):
-        await session.send(f"↓ 刚刚 [CQ:at,qq={session.event.user_id}] 撤回了以下消息ovo ↓\n{Message(recalled_message['message'])}")
+    is_prev_recall = settings['prevent_recall'].get(str(session.event.group_id), False)
+    if (str(session.event.user_id) != str(session.event.operator_id)) and is_prev_recall:
+        return await session.send(f"刚刚 [CQ:at,qq={session.event.operator_id}] 撤回了一条{'我' if session.event.user_id == session.self_id else '别人'}的消息ovo")
+    if is_prev_recall:
+        await session.send(f"↓ 刚刚 [CQ:at,qq={session.event.operator_id}] 撤回了以下消息ovo ↓\n{Message(recalled_message['message'])}")
 
 # 将函数注册为群成员增加通知处理器
 @on_notice('group_increase')

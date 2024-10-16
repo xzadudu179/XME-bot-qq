d944358b11c30065e2c60184bd7eb5b01361dd25
qfys521 <qfys520@vip.qq.com>
2024-10-16 22:32:00 +0800

    feat: add "setu" command.

---
+++ xme/plugins/commands/setu.py
@@ -0,0 +1,54 @@
+import requests
+from nonebot import on_command, CommandSession
+
+from xme.xmetools.doc_gen import CommandDoc
+
+alias = ["涩图" , "setu" , "色图" ]
+__plugin_name__ = 'setu'
+
+__plugin_usage__= str(CommandDoc(
+    name=__plugin_name__,
+    desc='涩图',
+    introduction='查看涩图',
+    usage=f'setu',
+    permissions=[],
+    alias=alias
+))
+
+@on_command(__plugin_name__, aliases=alias)
+async def setu(session: CommandSession):
+    api_url = "https://api.lolicon.app/setu/v2?r18=0&excludeAI=true"
+    result = fetch_image_data(api_url)
+    if result:
+        (session.send
+         (f"""
+         图片标题: {result.title}
+         图片pid: {result.pid}
+         作者: {result.author}
+         -----------
+         [CQ:Image , url={result.url}]
+        """))
+
+
+class ImageData:
+    def __init__(self, title, url, pid, author):
+        self.title = title
+        self.url = url
+        self.pid = pid
+        self.author = author
+def fetch_image_data(url):
+    response = requests.get(url)
+    data = response.json()
+
+    if data.get('error'):
+        print(f"Error: {data['error']}")
+        return None
+
+    image_data = data['data'][0]
+    return ImageData(
+        title=image_data['title'],
+        url=image_data['urls']['original'],
+        pid=image_data['pid'],
+        author=image_data['author']
+    )
+

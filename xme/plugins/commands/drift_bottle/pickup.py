bb1186c66482e93acb257963cac0ba6bd16ebad6
xzadudu179 <terra179@163.com>
2024-10-17 17:54:06 +0800

    修复了很严重的漂流瓶问题 呜呜

--- xme/plugins/commands/drift_bottle/pickup.py
+++ xme/plugins/commands/drift_bottle/pickup.py
@@ -10,11 +10,12 @@ from nonebot.command import call_command, CommandManager, Command
 
 pickup_alias = ["捡瓶子", "捡漂流瓶", "捡瓶", "pick"]
 
+
 @on_command('pickup', aliases=pickup_alias, only_to_me=False)
 async def _(session: CommandSession):
     user_id = session.event.user_id
     bottles_dict = json_tools.read_from_path('./data/drift_bottles.json')
-    bottles: list = bottles_dict['bottles']
+    bottles = bottles_dict['bottles']
     # 没捡到瓶子
     if len(bottles) < 1:
         await session.send("海里一个瓶子里都没有...")
@@ -25,13 +26,15 @@ async def _(session: CommandSession):
         return
 
     print("捡瓶子中")
-    bottle = random.choice(bottles)
+    # print(bottles)
+    # bottle = random.choice(bottles)
+    index, bottle = random.choice(list(bottles.items()))
     # 增加浏览量以及构造卡片
     # ----------------------------
     view_message = f"被捡到了{bottle['views'] + 1}次" if (bottle['views'] + 1) > 1 else f"第一次被捡到"
     bottle['views'] += 1
     like_message = f"获得了{bottle['likes']}个赞owo" if bottle['likes'] > 0 else f"还没有任何赞ovo"
-    bottle_card = f"[CQ:at,qq={user_id}] 你捡到了一个漂流瓶~\n[#{bottle['index']}号漂流瓶，来自 \"{bottle['from_group']}\"]：\n-----------\n{bottle['content']}\n-----------\n由 \"{bottle['sender']}\" 在{bottle['send_time']} 投出\n这个瓶子{view_message}，{like_message}"
+    bottle_card = f"[CQ:at,qq={user_id}] 你捡到了一个漂流瓶~\n[#{index}号漂流瓶，来自 \"{bottle['from_group']}\"]：\n-----------\n{bottle['content']}\n-----------\n由 \"{bottle['sender']}\" 在{bottle['send_time']} 投出\n这个瓶子{view_message}，{like_message}"
     # ----------------------------
     # 手滑摔碎了瓶子 (1% 概率)
     broken = (random.randint(0, 100) > 99)
@@ -43,7 +46,11 @@ async def _(session: CommandSession):
     if broken:
         print("瓶子碎了")
         await session.send("啊，你不小心把瓶子摔碎了...")
-        del bottles_dict['bottles'][bottle['index'] - 1]
+        bottles_dict = json_tools.read_from_path('./data/drift_bottles.json')
+        del bottles_dict['bottles'][index]
+        print("保存文件中")
+        json_tools.save_to_path('./data/drift_bottles.json', bottles_dict)
+        return
     else:
         # 处理之后可能的输入
         # 时间可能较长 先保存
@@ -56,13 +63,13 @@ async def _(session: CommandSession):
             content = "点赞成功~"
             bottles_dict = json_tools.read_from_path('./data/drift_bottles.json')
             print("重新读取")
-            bottles_dict['bottles'][bottle['index'] - 1]['likes'] += 1
+            bottles_dict['bottles'][index]['likes'] += 1
             print("点赞了")
-            print(bottles_dict['bottles'][bottle['index'] - 1])
+            print(bottles_dict['bottles'][index])
         elif reply == '-rep':
             content = "举报成功"
             for superuser in config.SUPERUSERS:
-                await session.bot.send_private_msg(user_id=superuser,message=f"{(await session.bot.get_group_member_info(group_id=session.event.group_id, user_id=user_id))['nickname']} ({user_id}) 举报了一个漂流瓶，瓶子信息如下：\n内容：\n-----------\n{bottle['content']}\n-----------\nIndex: {bottle['index']}\n发送者: {bottle['sender']} ({bottle['sender_id']})\n来自群：{bottle['from_group']} ({bottle['group_id']})")
+                await session.bot.send_private_msg(user_id=superuser,message=f"{(await session.bot.get_group_member_info(group_id=session.event.group_id, user_id=user_id))['nickname']} ({user_id}) 举报了一个漂流瓶，瓶子信息如下：\n内容：\n-----------\n{bottle['content']}\n-----------\nIndex: {index}\n发送者: {bottle['sender']} ({bottle['sender_id']})\n来自群：{bottle['from_group']} ({bottle['group_id']})")
         else:
             await send_cmd(reply, session)
             return
@@ -70,7 +77,7 @@ async def _(session: CommandSession):
             # bottles_dict = json_tools.read_from_path('./data/drift_bottles.json')
             # 重新读取
     print("保存文件中")
-    print(bottles_dict['bottles'][bottle['index'] - 1])
+    print(bottles_dict['bottles'][index])
     # print(bottle)
     json_tools.save_to_path('./data/drift_bottles.json', bottles_dict)
     await session.send(content)

70b4fb190531d4a8cfa3e2b6503a67e99244493d
xzadudu179 <terra179@163.com>
2024-10-19 15:41:29 +0800

    更新一下xme类

--- xme/plugins/commands/xme/database.py
+++ xme/plugins/commands/xme/database.py
@@ -1,6 +1,7 @@
 import sqlite3
 from xme.xmetools import date_tools
 from nonebot import log
+from functools import wraps
 
 class Xme_database:
     """XME 数据库相关类
@@ -10,19 +11,22 @@ class Xme_database:
         """
         self.db_path = "./data/xme/xme.db"
 
-    def database_control(self, func):
-        def wrapper(*args, **kwargs):
+    def database_control(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
             try:
                 # 初始化局部变量
+                print("初始化")
                 connection = sqlite3.connect(self.db_path)
                 cursor = connection.cursor()
                 # 将变量传递给原始函数
-                result = func(cursor, *args, **kwargs)
+                result = func(self, cursor, *args, **kwargs)
                 connection.commit()
             except Exception as ex:
                 log.logger.error(f"ERROR: XME 注册/签到出现问题: {ex}\n{ex.with_traceback()}")
                 connection.rollback()
             finally:
+                print("连接结束，关闭连接")
                 connection.close()
                 return result
         return wrapper
@@ -44,15 +48,28 @@ class Xme_database:
         ''')
 
     @database_control
-    def save_user_info(self, cursor, user):
+    def exec_query(self, cursor, query, params=()):
+        """查询数据库内容
+
+        Args:
+            query (str): 查询sql语句
+            params (tuple, optional): 查询占位符所对应内容. Defaults to ().
+
+        Returns:
+            list[tuple]: 查询结果
+        """
+        cursor.execute(query, params)
+        return cursor.fetchall()
+
+    def save_user_info(self, user):
         """保存用户数据 (还没写完)
 
         Args:
             cursor (Cursor): 数据库 cursor
             user (User): 用户数据
         """
-        # cursor.execute('''
-        #     UPDATE users
-        #     SET last_reg_days = ?, coins = ?
-        #     WHERE id = ?
-        # ''', (date_tools.curr_days(), coins, user_id))
+        self.exec_query('''
+            UPDATE users
+            SET last_reg_days = ?, coins = ?
+            WHERE id = ?
+        ''', (user.last_reg_days, user.coins, user.id))
--- xme/plugins/commands/xme/user.py
+++ xme/plugins/commands/xme/user.py
@@ -1,16 +1,40 @@
 from .database import Xme_database
+from xme.xmetools import date_tools
+from functools import wraps
+
 
 class User:
     """XME 用户类
     """
     MAX_NAME_LENGTH = 20
-    def __init__(self, name: str, last_reg_days: int, coins: int, database: Xme_database) -> None:
+    def __init__(self,id: int, name: str, coins: int, database: Xme_database) -> None:
+        """初始化用户
+
+        Args:
+            id (int): 用户 id （建议为 qq 号）
+            name (str): 用户名
+            coins (int): _description_
+            database (Xme_database): _description_
+
+        Raises:
+            ValueError: _description_
+        """
         if len(name) > self.MAX_NAME_LENGTH:
             raise ValueError(f"name 参数长度过长 (>{self.MAX_NAME_LENGTH})")
+        self.id = id
         self.name = name
-        self.last_reg_days = last_reg_days
+        self.last_reg_days = 0
         self.coins = coins
         self.database = database
+
+    def load_user(database: Xme_database, user_id: int):
+        """通过数据库表寻找并且返回用户
+
+        Args:
+            database (Xme_database): 数据库
+            user_id (int): 用户id
+        """
+        pass
 
     def change_name(self, new_name) -> bool:
         if len(new_name) > self.MAX_NAME_LENGTH:
@@ -18,19 +42,27 @@ class User:
         self.name = new_name
         return
 
-    def update_user_data(self, func, database):
+    def update_user_data(func):
         """用户数据更新装饰函数
 
         Args:
             func (_type_): 装饰函数
         """
-        def wrapper(*args, **kwargs):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
             result = func(*args, **kwargs)  # 调用原始函数
             # 在函数执行完毕后执行的指令
-            database.save_user_info(self)
+            self.database.save_user_info(self)
             print("用户数据已更新.")
             return result
         return wrapper
 
-    def register(self):
+    @update_user_data
+    def try_register(self) -> bool:
+        self.database.init_user_info()
+        curr_days = date_tools.curr_days()
+        if self.last_reg_days >= curr_days:
+            return False
+        self.last_reg_days = curr_days
+        # self.database.save_user_info(user=self)
         pass

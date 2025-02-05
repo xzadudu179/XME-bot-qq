77e36fcb27af00954d175e2ca7629caeca031d90
xzadudu179 <terra179@163.com>
2025-02-06 05:45:12 +0800

    update

--- xme/plugins/event_parsers/notice/group_increase.py
+++ xme/plugins/event_parsers/notice/group_increase.py
@@ -3,7 +3,7 @@ from character import get_message
 from xme.xmetools.command_tools import send_session_msg
 import asyncio
 
-increase_people = []
+increase_people = {}
 
 # 将函数注册为群成员增加通知处理器
 @on_notice('group_increase')
@@ -13,13 +13,15 @@ async def _(session: NoticeSession):
     # 如果是自己换一种欢迎方法
     if session.event.user_id == session.self_id:
         return await send_session_msg(session, get_message("event_parsers", "welcome_self"), at=False)
-    # 1.5 秒内加群的人会被一起计算
-    increase_people.append(f"[CQ:at,qq={session.event.user_id}]")
+    # 指定秒内加群的人会被一起计算
+    group = session.event.group_id
+    increase_people[group] = increase_people.get(group, [])
+    increase_people[group].append(f"[CQ:at,qq={session.event.user_id}]")
     # print(increase_people)
-    people = increase_people.copy()
-    await asyncio.sleep(1.5)
+    people = increase_people[group]
+    await asyncio.sleep(2)
     # print(increase_people)
-    if len(people) == len(increase_people):
-        await send_session_msg(session, get_message("event_parsers", "welcome", at=" ".join(increase_people)), False)
-        increase_people = []
+    if len(people) == len(increase_people[group]):
+        await send_session_msg(session, get_message("event_parsers", "welcome", at=" ".join(increase_people[group])), False)
+        increase_people[group] = []
         return

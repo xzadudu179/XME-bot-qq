b9d7b62f0def3ec8d7bdef645501672fcdd979ee
xzadudu179 <terra179@163.com>
2025-07-31 00:43:09 +0800

    不小心的语法问题

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ PEEK_GROUP = [
 ]
 
 NICKNAME = character.get_character_item('bot_info', 'nickname', default=['XME', 'xme'])
-VERSION = '0.4.10'
+VERSION = '0.4.14'
 
 USER_PATH = "./data/users.json"
 BOT_SETTINGS_PATH = "./data/_botsettings.json"
